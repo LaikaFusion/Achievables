a464bcbd0ba87272be58fc1de7f7f0746aba1b5b
koennecker <koenneckerb@gmail.com>
2019-01-02 13:27:57 -0700

    Create tables for database.

--- server/database/migrations/20190102123137_achievements.js
+++ server/database/migrations/20190102123137_achievements.js
@@ -1,8 +1,18 @@
+exports.up = knex =>
+  knex.schema.createTable("achievements", achievements => {
+    achievements.increments();
+    achievements
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users");
+    achievements
+      .string("title", 128)
+      .notNullable()
+      .unique();
+    achievements.text("description").notNullable();
+    achievements.string("image").notNullable();
+  });
 
-exports.up = function(knex, Promise) {
-  
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = knex => knex.schema.dropTableIfExists("achievements");
