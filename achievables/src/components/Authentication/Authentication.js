b3efd7487fc2d2ac8f49066b32d7cedbb19c49f6
pedro <pedroluismontesinosrivera@gmail.com>
2019-01-02 16:42:54 -0500

    done with register and login components

---
+++ achievables/src/components/Authentication/Authentication.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Register from './Register'
+import Login from './Login';
+import { withRouter, NavLink, Switch, Route } from 'react-router-dom';
+
+const url = process.env.REACT_APP_API_URL;
+
+class Authentication extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: false,
+            users: []
+        }
+    }
+
+    authenticate = () => {
+        const token = localStorage.getItem('secret_token');
+        const options = {
+            headers: {
+                authentication: token
+            }
+        }
+
+        if (token) {
+            axios.get(`${url}/api/users`, options)
+                .then(response => {
+                    if (response.status === 200 && response.data) {
+                        this.setState({ loggedIn: true, users: response.data });
+                    } else {
+                        throw new Error();
+                    }
+                })
+                .catch(error => {
+                    this.props.history.push('/login');
+                })
+        } else {
+            this.props.history.push('/login')
+        }
+    }
+
+    componentDidMount() {
+        this.authenticate();
+    }
+
+    componentDidUpdate(prevProps) {
+        const { pathname } = this.props.location;
+        console.log(this.props);
+        console.log(prevProps);
+
+        if (pathname === '/' && pathname !== prevProps.location.pathname) {
+            this.authenticate();
+        }
+    }
+
+    render() {
+        return (
+            <div className="Authentication">
+                <nav>
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='/login'>Login</NavLink>
+                    <NavLink to='/register'>register</NavLink>
+                </nav>
+
+                <section>
+                    <Switch>
+                        <Route path="/register" component={Register} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/" render={() => {
+                            return (
+                                <React.Fragment>
+                                    <h1>Users</h1>
+                                    <div>
+                                        {this.state.users.map(user => <p key={user.id}>{user.username}</p>)}
+                                    </div>
+                                </React.Fragment>
+                            )
+                        }} />
+                    </Switch>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default withRouter(Authentication);
---
+++ achievables/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const LandingPage = () => (
+    <div className="landing-page">
+        <h1>This is the landing page</h1>
+    </div>
+);
+
+export default LandingPage;
